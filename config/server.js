b47be4a66bb222bd0fab9221168a79d5abd905a4
hscheffert <hscheffert@qci.com>
2021-03-05 15:44:33 -0600

    init commit for gatsby + strapi blog tutorial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3d16ed680a4eb248bd81edd05139a231'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fe44d212-d7f4-4f1e-a107-aafc03a6be3a'
+};
